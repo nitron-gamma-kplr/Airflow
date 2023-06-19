960f43af275b9a4d145dfe022ac38ffc72f2c09b
nitron-gamma-kplr <130391711+nitron-gamma-kplr@users.noreply.github.com>
2023-06-19 12:10:25 +0000

    V2

--- dags/dag1.py
+++ dags/dag1.py
@@ -12,14 +12,19 @@ def hello_world():
 
 # Define the DAG
 dag = DAG(
-    "hello_world_dag_minute_time",
+    "hello_world_dag",
     description="A simple DAG that prints Hello, World!",
-    start_date=datetime(2023, 6, 19, 11, 58, 0),
-    schedule_interval="* * * * *",
+    start_date=datetime(2023, 5, 1),
+    schedule_interval="@once",
 )
 
 # Define the task
 hello_task = PythonOperator(task_id="hello_task", python_callable=hello_world, dag=dag)
 
+# Define the task
+hello_task2 = PythonOperator(
+    task_id="hello_task2", python_callable=hello_world, dag=dag
+)
+
 # Set the task dependencies
-hello_task
+hello_task >> hello_task2
