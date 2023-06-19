2fbdca9bd2b6a788883128bedd3949c4f2080179
nitron-gamma-kplr <130391711+nitron-gamma-kplr@users.noreply.github.com>
2023-06-19 12:06:46 +0000

    V2

--- dags/dag1.py
+++ dags/dag1.py
@@ -2,33 +2,24 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 
+
 def hello_world():
     print("#############################################")
     print("  Hello, AIRFLOW!")
-    print((datetime.now()+timedelta(hours=2)).strftime("%y-%m-%d %H:%M:%S"))
+    print((datetime.now() + timedelta(hours=2)).strftime("%y-%m-%d %H:%M:%S"))
     print("#############################################")
+
 
 # Define the DAG
 dag = DAG(
-    'hello_world_dag',
-    description='A simple DAG that prints Hello, World!',
-    start_date=datetime(2023, 5, 1),
-    schedule_interval='@once'
+    "hello_world_dag_minute_time",
+    description="A simple DAG that prints Hello, World!",
+    start_date=datetime(2023, 6, 19, 11, 58, 0),
+    schedule_interval="* * * * *",
 )
 
 # Define the task
-hello_task = PythonOperator(
-    task_id='hello_task',
-    python_callable=hello_world,
-    dag=dag
-)
+hello_task = PythonOperator(task_id="hello_task", python_callable=hello_world, dag=dag)
 
 # Set the task dependencies
 hello_task
-
-
-
-
-
-
-
